caac0b8a950e2e31d2b6e7ee1354201742c75acc
Erfan Abdi <erfangplus@gmail.com>
2019-09-26 19:40:20 +0330

    nicky: Write Fingerprint HAL Adapter
    vendor.oppo.hardware.biometrics.fingerprint@2.1 -> android.hardware.biometrics.fingerprint@2.1
    
    Change-Id: Id33db73425bb5e92ef5c45e2f8d40fa462942af3
    Signed-off-by: SagarMakhar <sagarmakhar@gmail.com>

--- device.mk
+++ device.mk
@@ -179,7 +179,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.oplus
+    android.hardware.biometrics.fingerprint@2.1-service.realme_sdm710
 
 # Gatekeeper
 PRODUCT_PACKAGES += \
