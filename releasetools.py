50d6c96e3c196294642436de8ca823c05af0a676
pjgowtham <pjgowtham@gmail.com>
2022-07-29 07:21:32 +0530

    nicky: Fix mount errors for system with TWRP post install

--- releasetools.py
+++ releasetools.py
@@ -38,4 +38,5 @@ def OTA_InstallEnd(info):
   info.script.Print("Patching firmware images...")
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
+  info.script.AppendExtra('mount("ext4", "EMMC", "/dev/block/by-name/system", "/system_root", "");');
   return
