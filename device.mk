d0cfc8ea17ad8c66ed0c4a67567dd32d93b65425
Adam Lawson <dev@goshawk22.uk>
2022-09-04 17:13:58 +0100

    nicky: Build     android.hardware.secure_element@1.2.vendor
    [09-04 16:02:36.604 1874:1874 F/linker]
    CANNOT LINK EXECUTABLE "/vendor/bin/hw/qcrild": library "android.hardware.secure_element@1.2.so" not found: needed by /vendor/lib64/libril-qc-hal-qmi.so in namespace (default)

--- device.mk
+++ device.mk
@@ -325,6 +325,10 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.3.vendor \
     android.hardware.radio.deprecated@1.0.vendor
 
+# Secure Element
+PRODUCT_PACKAGES += \
+    android.hardware.secure_element@1.2.vendor
+
 # Sensors
 PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-service.multihal \
