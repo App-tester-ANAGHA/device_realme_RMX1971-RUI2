a4e40eddaf2bac67346aa65db945e520f1ce90f1
Adam Lawson <dev@goshawk22.uk>
2022-09-11 12:36:55 +0100

    nicky: Switch to basic usb hal
    USB control (for mtp etc) wasn't working unless developer options and usb debugging was enabled. Use basic usb hal to fix this

--- device.mk
+++ device.mk
@@ -363,7 +363,7 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
+    android.hardware.usb@1.0-service.basic
 
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
